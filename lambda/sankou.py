4470db7f64fc93c6b9ae422dea46a02c00e0bbaa
kurokawa111 <kurokawa111@gmail.com>
2023-03-27 18:07:06 +0900

    日本語表示に変更

--- lambda/sankou.py
+++ lambda/sankou.py
@@ -2,8 +2,8 @@ def is_prime(n: int) -> bool:
     if n <= 1:
         return False
 
-    # if n == 2:
-    #     return True
+    if n == 2:
+        return True
 
     if n % 2 == 0:
         return False
